94717d54e527a9980be113eeae399c4a97cdb4c1
Clément Decoodt <clement@decoodt.eu>
2020-03-28 15:51:14 +0100

    Add basic cli client on server

--- server/src/cli.rs
+++ server/src/cli.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use proto::extra_client::ExtraClient;
+use proto::ServiceInfoRequest;
+
+pub mod proto {
+    tonic::include_proto!("gameapi");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = ExtraClient::connect("https://[::1]:50051").await?;
+
+    let request = tonic::Request::new(ServiceInfoRequest{});
+    let response = client.service_info(request).await?;
+
+    println!("RESPONSE: {:?}", response);
+
+    Ok(())
 }
