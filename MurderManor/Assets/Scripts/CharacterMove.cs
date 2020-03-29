8c7d24de29033222f1c4e47e69d037ffcbe2231c
Clément Decoodt <clement@decoodt.eu>
2020-03-29 01:43:30 +0100

    Apply clang-formatter on CharacterMove+PlayersManager
    This commit has no functionnal change

--- MurderManor/Assets/Scripts/CharacterMove.cs
+++ MurderManor/Assets/Scripts/CharacterMove.cs
@@ -4,42 +4,38 @@ using System.Collections;
 
 // This script moves the character controller forward
 // and sideways based on the arrow keys.
-// It also jumps when pressing space.
 // Make sure to attach a character controller to the same game object.
-// It is recommended that you make only one call to Move or SimpleMove per frame.
+// It is recommended that you make only one call to Move or SimpleMove per
+// frame.
 
-public class CharacterMove : MonoBehaviour
-{
+public class CharacterMove : MonoBehaviour {
     CharacterController characterController;
 
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
-    [SerializeField]  private Animator m_Animator;
+    [SerializeField]
+    private Animator m_Animator;
 
     private Vector3 moveDirection = Vector3.zero;
 
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();
-    }
+    void Start() { characterController = GetComponent<CharacterController>(); }
 
-    void Update()
-    {
-
-
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        if (moveDirection != Vector3.zero)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection.normalized), 0.5f);
+    void Update() {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f,
+                                    Input.GetAxis("Vertical"));
+        if (moveDirection != Vector3.zero) {
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(moveDirection.normalized), 0.5f);
             m_Animator.SetBool("Walk", true);
         }
 
-        else m_Animator.SetBool("Walk", false);
+        else
+            m_Animator.SetBool("Walk", false);
 
         moveDirection *= speed;
         characterController.Move(moveDirection * Time.deltaTime);
-
     }
 }
 
--- MurderManor/Assets/Scripts/PlayersManager.cs
+++ MurderManor/Assets/Scripts/PlayersManager.cs
@@ -3,37 +3,38 @@ using Gameapi;
 using UnityEngine;
 
 public class PlayersManager : MonoBehaviour {
-   private Game.GameClient _client;
-   private Grpc.Core.Channel _grpc_channel;
+    private Game.GameClient _client;
+    private Grpc.Core.Channel _grpc_channel;
 
-   string endpoint = "[::1]:50051";
+    string endpoint = "[::1]:50051";
 
-   private void Start() {
-      _grpc_channel = new Grpc.Core.Channel(endpoint,
-            Grpc.Core.ChannelCredentials.Insecure);
-      _client = new Game.GameClient(_grpc_channel);
-   }
+    private void Start() {
+        _grpc_channel = new Grpc.Core.Channel(
+            endpoint, Grpc.Core.ChannelCredentials.Insecure);
+        _client = new Game.GameClient(_grpc_channel);
+    }
 
-   public string NewPlayer(string name) {
-      var player = _client.NewPlayer(new NewPlayerRequest { Name = name });
-      return player.Id;
-   }
+    public string NewPlayer(string name) {
+        var player = _client.NewPlayer(new NewPlayerRequest{Name = name});
+        return player.Id;
+    }
 
-   public void MovePlayer(string id, UnityEngine.Vector2 position, UnityEngine.Vector2 direction) {
-      _client.MovePlayer(new MovePlayerRequest {
+    public void MovePlayer(string id, UnityEngine.Vector2 position,
+                           UnityEngine.Vector2 direction) {
+        _client.MovePlayer(new MovePlayerRequest{
             Id = id,
-            NewCoordinates = new Gameapi.Vector2{
-               X = position.x,
-               Y = position.y,
-            },
-            NewDirection = new Gameapi.Vector2{
-               X = direction.x,
-               Y = direction.y,
-            },
-            });
-   }
+            NewCoordinates =
+                new Gameapi.Vector2{
+                    X = position.x,
+                    Y = position.y,
+                },
+            NewDirection =
+                new Gameapi.Vector2{
+                    X = direction.x,
+                    Y = direction.y,
+                },
+        });
+    }
 
-   private void OnDisable() {
-      _grpc_channel.ShutdownAsync().Wait();
-   }
+    private void OnDisable() { _grpc_channel.ShutdownAsync().Wait(); }
 }
