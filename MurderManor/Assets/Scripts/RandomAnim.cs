b4e3f6b210eb4c7f4624165bc00a1b4e478d4f93
Elmephisto <sander.dumet@gmail.com>
2020-03-29 23:42:26 +0200

    Add props and start of object picker

---
+++ MurderManor/Assets/Scripts/RandomAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+
+        float randomIdleStart = Random.Range(0, anim.GetCurrentAnimatorStateInfo(0).length); //Set a random part of the animation to start from
+        anim.Play("BushIdle", 0, randomIdleStart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
