1555b73898e48f63b5cf249753ca60fe9a2f7620
Elmephisto <sander.dumet@gmail.com>
2020-03-30 01:08:45 +0200

    update object picker

--- MurderManor/Assets/Scripts/ObjectPicker.cs
+++ MurderManor/Assets/Scripts/ObjectPicker.cs
@@ -6,6 +6,9 @@ public class ObjectPicker : MonoBehaviour
 {
     
     private List<Collider> activeObjects = new List<Collider>();
+    public GameObject itemContainer;
+    private bool isHoldingItem = false;
+    private GameObject itemHeld;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class ObjectPicker : MonoBehaviour
        
         var closestObject = activeObjects[0].gameObject;
         var currentMinDist = Vector3.Distance(closestObject.gameObject.transform.position, transform.position);
+        
 
         foreach (var activeObject in activeObjects)
         {
@@ -35,12 +39,27 @@ public class ObjectPicker : MonoBehaviour
 
         if (Input.GetButtonDown("Pickup"))
         {
-            closestObject.transform.parent = transform;
-            //closestObject.transform.position = transform.position;
-            closestObject.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z + 0.2f);
-            closestObject.GetComponent<Rigidbody>().isKinematic = true;
-            Debug.Log(closestObject);
-        }
+            if (isHoldingItem == false)
+            {
+                isHoldingItem = true;
+                itemHeld = closestObject;
+                closestObject.transform.parent = transform.Find("ItemPosition").gameObject.transform;
+                closestObject.transform.localPosition = new Vector3(0, 0, 0);
+                //closestObject.transform.position = transform.position;
+                //closestObject.transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z + 0.2f);
+                closestObject.GetComponent<Rigidbody>().isKinematic = true;
+                Debug.Log(closestObject);
+
+            }
+
+            else
+            {
+                isHoldingItem = false;
+                itemHeld.transform.parent = itemContainer.transform;
+                closestObject.GetComponent<Rigidbody>().isKinematic = false;
+
+            }
+        }   
     }
 
     private void OnTriggerEnter(Collider other)
