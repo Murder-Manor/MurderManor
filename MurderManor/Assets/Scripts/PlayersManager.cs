3e34ad4df525a41e0686f812f7afe7260dc250fb
Clément Decoodt <clement@decoodt.eu>
2020-03-29 01:39:27 +0100

    Add Player Manager client

---
+++ MurderManor/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,39 @@
+using System;
+using Gameapi;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour {
+   private Game.GameClient _client;
+   private Grpc.Core.Channel _grpc_channel;
+
+   string endpoint = "[::1]:50051";
+
+   private void Start() {
+      _grpc_channel = new Grpc.Core.Channel(endpoint,
+            Grpc.Core.ChannelCredentials.Insecure);
+      _client = new Game.GameClient(_grpc_channel);
+   }
+
+   public string NewPlayer(string name) {
+      var player = _client.NewPlayer(new NewPlayerRequest { Name = name });
+      return player.Id;
+   }
+
+   public void MovePlayer(string id, UnityEngine.Vector2 position, UnityEngine.Vector2 direction) {
+      _client.MovePlayer(new MovePlayerRequest {
+            Id = id,
+            NewCoordinates = new Gameapi.Vector2{
+               X = position.x,
+               Y = position.y,
+            },
+            NewDirection = new Gameapi.Vector2{
+               X = direction.x,
+               Y = direction.y,
+            },
+            });
+   }
+
+   private void OnDisable() {
+      _grpc_channel.ShutdownAsync().Wait();
+   }
+}
