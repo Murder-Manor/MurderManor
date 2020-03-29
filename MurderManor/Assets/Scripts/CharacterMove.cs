eedb7d1eba9c5d852f48c6a463b2bb9204f3ae5f
Elmephisto <sander.dumet@gmail.com>
2020-03-29 01:05:51 +0100

    Update character controller

--- MurderManor/Assets/Scripts/CharacterMove.cs
+++ MurderManor/Assets/Scripts/CharacterMove.cs
@@ -15,6 +15,7 @@ public class CharacterMove : MonoBehaviour
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    [SerializeField]  private Animator m_Animator;
 
     private Vector3 moveDirection = Vector3.zero;
 
@@ -25,27 +26,20 @@ public class CharacterMove : MonoBehaviour
 
     void Update()
     {
-        if (characterController.isGrounded)
+
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        if (moveDirection != Vector3.zero)
         {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection.normalized), 0.5f);
+            m_Animator.SetBool("Walk", true);
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+        else m_Animator.SetBool("Walk", false);
 
-        // Move the controller
+        moveDirection *= speed;
         characterController.Move(moveDirection * Time.deltaTime);
+
     }
 }
 
