75740cebcc94188231800c3fcccc7f398b3aad14
Clément Decoodt <clement@decoodt.eu>
2020-04-11 17:39:51 +0200

    Use MovePosition in FixedUpdate for characterController

--- MurderManor/Assets/Scripts/CharacterMove.cs
+++ MurderManor/Assets/Scripts/CharacterMove.cs
@@ -8,15 +8,16 @@ using System.Collections;
 // It is recommended that you make only one call to Move per frame.
 
 public class CharacterMove : MonoBehaviour {
-    public float speed = 1.0f;
     public string id;
 
     // These variables must be set in unity interface
     public Animator m_Animator = null;
+    public float speed = 1.0f;
 
     private float lastUpdatedTime = 0.0f;
     private string characterName;
     private uint score = 0;
+    private Vector3 moveDirection = Vector3.zero;
 
     public void SetCharacterName(string characterName) {
         this.characterName = characterName;
@@ -38,10 +39,19 @@ public class CharacterMove : MonoBehaviour {
 
     void Update() {
         // Normalization also ensures a "small" vector will be set to vector3.zero
-        var moveDirection = Vector3.Normalize(
+        moveDirection = Vector3.Normalize(
                new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")));
-        MoveTo(moveDirection);
         RotateTo(moveDirection);
+    }
+
+    void FixedUpdate() {
+        // Move the player using a MovePosition instead of adding a force and let
+        // the physics engine do stuff for us, thus avoiding different speeds
+        // between players depending on local physics engine resolution.
+        // Doing it in FixedUpdate lets the move be done before physics engine
+        // resolution.
+        var rb = GetComponent<Rigidbody>();
+        SetPosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
     }
 
     public Vector3 GetPosition() {
@@ -64,16 +74,10 @@ public class CharacterMove : MonoBehaviour {
                 Quaternion.LookRotation(dir.normalized), 0.5f);
     }
 
-    public void MoveTo(Vector3 dir) {
-        lastUpdatedTime = Time.time;
-        m_Animator.SetBool("Walk", dir != Vector3.zero);
-        GetComponent<Rigidbody>().AddForce(dir * speed);
-    }
-
     public void SetPosition(Vector3 pos) {
         m_Animator.SetBool("Walk", GetComponent<Rigidbody>().position != pos);
         lastUpdatedTime = Time.time;
-        GetComponent<Rigidbody>().position = pos;
+        GetComponent<Rigidbody>().MovePosition(pos);
     }
 
     public void SetDirection(Vector3 dir) {
