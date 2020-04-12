a924f1a3505a3ff86d8c479dda95514e7a1f401f
Clément Decoodt <clement@decoodt.eu>
2020-04-12 22:14:37 +0200

    Change object data structure to HashSet
    The goal is to avoid same player to take twice an item

--- server/src/objects.rs
+++ server/src/objects.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::collections::{
+    HashMap,
+    HashSet,
+};
 use std::error;
 
 use rand::seq::SliceRandom;
@@ -7,7 +10,7 @@ use uuid::Uuid;
 #[derive(Default, Debug)]
 pub struct Object {
     pub uuid: Uuid,
-    pub taken_by: Vec<Uuid>,
+    pub taken_by: HashSet<Uuid>,
 }
 
 #[derive(Default)]
@@ -21,29 +24,29 @@ impl Objects {
         self.takable_objects.choose(&mut rand::thread_rng()).unwrap().clone()
     }
 
-    pub fn takers_for(&self, object_id: Uuid) -> Vec<Uuid> {
+    pub fn takers_for(&self, object_id: Uuid) -> HashSet<Uuid> {
         match self.objects.get(&object_id) {
             Some(object) => object.taken_by.clone(),
-            None => return vec![],
+            None => return HashSet::<Uuid>::new(),
         }
     }
 
-    // Take an object and return the position of the player in the takers
-    pub fn take_object(&mut self, object_id: Uuid, player_id: Uuid) -> Result<usize, Box<dyn error::Error>> {
+    // Take an object
+    pub fn take_object(&mut self, object_id: Uuid, player_id: Uuid) -> Result<(), Box<dyn error::Error>> {
         let object = match self.objects.get_mut(&object_id) {
             Some(object) => object,
             None => {
                 let object = Object {
                     uuid: object_id,
-                    taken_by: vec![],
+                    taken_by: HashSet::<Uuid>::new(),
                 };
                 self.objects.insert(object_id, object);
                 self.objects.get_mut(&object_id).unwrap()
             }
         };
 
-        object.taken_by.push(player_id);
-        Ok(object.taken_by.len() - 1)
+        object.taken_by.insert(player_id);
+        Ok(())
     }
 
     pub fn reset(&mut self) {
